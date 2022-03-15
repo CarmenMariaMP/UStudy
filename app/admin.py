a92f7a0a27dc62dd862df014c0314749defe1bdf
juanramonuvus <72406617+juanramonuvus@users.noreply.github.com>
2022-03-15 20:44:22 +0100

    backend: Creación de entidades
    Se crean los siguientes modelos con sus respectivas relaciones:
    - Archivo
    - Asignatura
    - Comentario
    - Curso
    - Reporte
    - Usuario
    - Valoración
    - Notificación

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from app.models import *
+
 
 # Register your models here.
+admin.site.register(Asignatura)
+admin.site.register(Usuario)
+admin.site.register(Curso)
+admin.site.register(Archivo)
+admin.site.register(Comentario)
+admin.site.register(Notificacion)
+admin.site.register(Valoracion)
+admin.site.register(Reporte)
