d12d8cc2dde10ac6b705765b1b2f8cb7d976f66a
Daniel Rodriguez Camacho <danrodcam@alum.us.es>
2022-03-19 21:16:32 +0100

    backend: Zona curso profesor
    Se construye el backend para los cursos que ha creado un profesor

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from app.models import Usuario
+from app.models import Archivo, Usuario,Curso, Valoracion
 
 # Create your views here.
 def inicio(request):
@@ -28,8 +28,33 @@ def logout_user(request):
     return render(request, "inicio.html")
 
 def inicio_profesor(request):
+
+
+    usuarioActual = request.user.usuario
+
+    cursosUsuario = Curso.objects.all().filter(propietario=usuarioActual).order_by('nombre')
+
+    dicc= dict()
+
+    for curso in cursosUsuario:
+        archivos = Archivo.objects.all().filter(curso = curso)
+        valoraciones = Valoracion.objects.all().filter(curso=curso)
+        puntos = 0
+        for valoracion in valoraciones:
+            puntos += valoracion.puntuacion
+            
+        if len(valoraciones)>0:
+            mediaPuntos=puntos/len(valoraciones)
+        else:
+            mediaPuntos = "No tiene valoraciones"
+
+        dicc[curso] = (len(archivos),mediaPuntos,len(curso.suscriptores.all()))
+
+
+        
+
     if request.user.is_authenticated:
-        return render(request, "inicio_profesor.html", {'nombre': request.user.usuario.nombre})
+        return render(request, "inicio_profesor.html", {'nombre':usuarioActual.nombre, 'dicc':dicc})
     else:
         return render(request, 'inicio.html')
   
