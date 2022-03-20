2ad1abe6c6755d278ea02fac611853dba1da448b
Daniel Rodriguez Camacho <danrodcam@alum.us.es>
2022-03-20 20:31:05 +0100

    Backend Zona profesor
    Corrección de la redirección si no estas logueado

--- app/views.py
+++ app/views.py
@@ -28,33 +28,33 @@ def logout_user(request):
     return render(request, "inicio.html")
 
 def inicio_profesor(request):
-
-
-    usuarioActual = request.user.usuario
-
-    cursosUsuario = Curso.objects.all().filter(propietario=usuarioActual).order_by('nombre')
-
-    dicc= dict()
-
-    for curso in cursosUsuario:
-        archivos = Archivo.objects.all().filter(curso = curso)
-        valoraciones = Valoracion.objects.all().filter(curso=curso)
-        puntos = 0
-        for valoracion in valoraciones:
-            puntos += valoracion.puntuacion
-            
-        if len(valoraciones)>0:
-            mediaPuntos=puntos/len(valoraciones)
-        else:
-            mediaPuntos = "No tiene valoraciones"
-
-        dicc[curso] = (len(archivos),mediaPuntos,len(curso.suscriptores.all()))
-
-
+    if request.user.is_authenticated:
         
 
-    if request.user.is_authenticated:
+        usuarioActual = request.user.usuario
+
+        cursosUsuario = Curso.objects.all().filter(propietario=usuarioActual).order_by('nombre')
+
+        dicc= dict()
+
+        for curso in cursosUsuario:
+            archivos = Archivo.objects.all().filter(curso = curso)
+            valoraciones = Valoracion.objects.all().filter(curso=curso)
+            puntos = 0
+            for valoracion in valoraciones:
+                puntos += valoracion.puntuacion
+            
+            if len(valoraciones)>0:
+                mediaPuntos=puntos/len(valoraciones)
+            else:
+                mediaPuntos = "No tiene valoraciones"
+
+            dicc[curso] = (len(archivos),mediaPuntos,len(curso.suscriptores.all()))
+
+
         return render(request, "inicio_profesor.html", {'nombre':usuarioActual.nombre, 'dicc':dicc})
+
+    
     else:
         return render(request, 'inicio.html')
   
