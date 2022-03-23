0558b7912ec634d2f3219a9e394a6634e4d1415a
Daniel Rodriguez Camacho <danrodcam@alum.us.es>
2022-03-23 18:13:18 +0100

    Update settings.py

--- ustudy/settings.py
+++ ustudy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://ustudysprint1.herokuapp.com",
+ALLOWED_HOSTS = ["ustudysprint1.herokuapp.com",
 "localhost",
 "127.0.0.1"]
 
