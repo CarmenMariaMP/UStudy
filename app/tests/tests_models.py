0d4e8a7083a4d437d5dcad1dfba4254e21c12bd1
Alejandro Barranco Ledesma <alebarled@alum.us.es>
2022-03-30 12:12:38 +0200

    backend: Pruebas CursoModelTests

--- app/tests/tests_models.py
+++ app/tests/tests_models.py
@@ -113,7 +113,6 @@ class UsuarioModelTests(TestCase):
 
     ## Email academico sin dominio de la US
     def test_email_academico_negative(self):
-
         user=User.objects.first()
         usuario = Usuario.objects.create(nombre='Nombre', apellidos='Apellidos', email='email@hotmail.com', email_academico='email@hotmail.com',titulacion='Titulación 1',descripcion='Descripcion 1', 
             foto='foto.jpg', dinero=12.42, django_user=user)
@@ -122,8 +121,6 @@ class UsuarioModelTests(TestCase):
             usuario.full_clean()
         self.assertTrue('email_academico' in dict(context.exception))
             
-
-
 
 class NotificacionModelTests(TestCase):
 
@@ -174,3 +171,43 @@ class ValoracionModelTests(TestCase):
         self.assertEquals(valoracion.puntuacion,5)
         self.assertEquals(valoracion.usuario,usuario)
         self.assertEquals(valoracion.curso,curso)
+
+class CursoModelTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        #Set up non-modified objects used by all test methods
+        user = User.objects.create(username='User1', password='pass')
+        usuario = Usuario.objects.create(nombre='Nombre1', apellidos='Apellidos', email='email@hotmail.com', email_academico='barranco@alum.us.es', titulacion='Titulación 1',descripcion='Descripcion 1', 
+        foto='foto.jpg', dinero=12.4, django_user=user)
+        asignatura = Asignatura.objects.create(nombre='Nombre1', titulacion='Titulacion1', anyo=2012)
+    
+    def test_crear_curso_positiva(self):
+        fecha = datetime.datetime.now().date()
+        Curso.objects.create(nombre="Curso1", descripcion="Descripcion1", fecha_publicacion=fecha, asignatura=Asignatura.objects.first(), propietario=Usuario.objects.first())
+
+        curso = Curso.objects.first()
+        self.assertEquals(curso.nombre,"Curso1")
+        self.assertEquals(curso.descripcion,"Descripcion1")
+        self.assertEquals(curso.fecha_publicacion,fecha)
+        self.assertEquals(curso.asignatura,Asignatura.objects.first())
+        self.assertEquals(curso.propietario,Usuario.objects.first())
+
+    ## Longitud de nombre mayor de 100 caracteres
+    def test_crear_curso_negative_nombre_longitud_max (self):
+        user=User.objects.first()
+        with self.assertRaises(Exception) as context:
+            fecha = datetime.datetime.now().date()
+            Curso.objects.create(nombre="a"*101, descripcion="Descripcion1", fecha_publicacion=fecha, asignatura=Asignatura.objects.first(), propietario=Usuario.objects.first())
+        self.assertTrue('el valor es demasiado largo para el tipo character varying(100)' in str(context.exception))
+
+
+    ## Longitud de descripcion mayor de 500 caracteres
+    def test_crear_curso_negative_descripcion_longitud_max (self):
+        user=User.objects.first()
+        with self.assertRaises(Exception) as context:
+            fecha = datetime.datetime.now().date()
+            Curso.objects.create(nombre="Curso1", descripcion="a"*501, fecha_publicacion=fecha, asignatura=Asignatura.objects.first(), propietario=Usuario.objects.first())
+        self.assertTrue('el valor es demasiado largo para el tipo character varying(500)' in str(context.exception))
+    
+
+
