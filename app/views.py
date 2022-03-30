adc6885edc39d8ba802da3b7650d1c11f77bc71d
Irene <i.dominguez@alumno.laudealtillo.com>
2022-03-30 20:26:50 +0200

    Corregir valoracion media profesor

--- app/views.py
+++ app/views.py
@@ -12,13 +12,16 @@ from decouple import config
 
 from django.core.paginator import Paginator
 
-def pagination(request,productos):
+
+def pagination(request, productos):
     paginator = Paginator(productos, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return page_obj
 
 # Create your views here.
+
+
 def inicio(request):
     if not request.user.is_authenticated:
         return render(request, "inicio.html")
@@ -157,6 +160,8 @@ def inicio_profesor(request):
 
         dicc = dict()
         val = 0
+        ac = 0
+        sum = 0
 
         for curso in cursosUsuario:
             archivos = Archivo.objects.all().filter(curso=curso)
@@ -168,14 +173,18 @@ def inicio_profesor(request):
 
             if len(valoraciones) > 0:
                 mediaPuntos = puntos/len(valoraciones)
+                sum += mediaPuntos
+                ac += 1
+
             else:
                 mediaPuntos = "No tiene valoraciones"
 
-            val += mediaPuntos / len(cursosUsuario)
             dicc[curso] = (len(archivos), mediaPuntos,
                            len(curso.suscriptores.all()))
+            if (ac > 0):
+                val = sum / ac
 
-        return render(request, "inicio_profesor.html", {'nombre': usuarioActual.nombre, 'dicc': dicc, 'val': val})
+        return render(request, "inicio_profesor.html", {'nombre': usuarioActual.nombre, 'dicc': dicc, 'val': val, 'ac':ac})
 
     else:
         return redirect("/login", {"mensaje_error": True})
@@ -281,7 +290,7 @@ def cursosdisponibles(request):
 
 def ver_archivo(request, id_curso, id_archivo):
     acceso = False
-    es_owner =False
+    es_owner = False
     es_plagio = False
     es_error = False
     curso = Curso.objects.get(id=id_curso)
@@ -298,9 +307,9 @@ def ver_archivo(request, id_curso, id_archivo):
         usuario = Usuario.objects.get(django_user=usuario_autenticado)
         if (curso.propietario == usuario):
             reportes = Reporte.objects.all().filter(archivo=archivo)
-            page_obj = pagination(request,reportes)
+            page_obj = pagination(request, reportes)
             acceso = True
-            es_owner =True
+            es_owner = True
         if (usuario in curso.suscriptores.all()):
             acceso = True
 
@@ -317,13 +326,14 @@ def ver_archivo(request, id_curso, id_archivo):
                 return redirect('/curso/'+str(id_curso)+'/archivo/'+str(id_archivo))
         else:
             form = ReporteForm()
-        return render(request, "archivo.html", {'pdf': archivo.ruta, 'curso': curso, 'archivo': archivo, 'contenido_curso': contenido_curso, 
-        'acceso': acceso, 'comentarios': comentarios, 'url': url, 'form': form, 'page_obj':page_obj,'es_owner': es_owner,
-        'es_plagio': es_plagio,'es_error': es_error})
+        return render(request, "archivo.html", {'pdf': archivo.ruta, 'curso': curso, 'archivo': archivo, 'contenido_curso': contenido_curso,
+                                                'acceso': acceso, 'comentarios': comentarios, 'url': url, 'form': form, 'page_obj': page_obj, 'es_owner': es_owner,
+                                                'es_plagio': es_plagio, 'es_error': es_error})
     else:
         return render(request, 'inicio.html')
 
-def eliminar_reporte(request, id_curso, id_archivo,id_reporte):
+
+def eliminar_reporte(request, id_curso, id_archivo, id_reporte):
     curso = Curso.objects.get(id=id_curso)
     if request.user.is_authenticated:
         # Comprobar si el usuario es profesor
@@ -333,6 +343,7 @@ def eliminar_reporte(request, id_curso, id_archivo,id_reporte):
             reporte = Reporte.objects.get(id=id_reporte)
             reporte.delete()
     return redirect('/curso/'+str(id_curso)+'/archivo/'+str(id_archivo))
+
 
 def subir_contenido(request):
     return render(request, "subir_contenido.html")
