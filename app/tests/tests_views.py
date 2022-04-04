fce158addb6b43ca0eafe819606a88777468740e
Alejandro Barranco Ledesma <alebarled@alum.us.es>
2022-04-04 15:48:31 +0200

    backend: Comienza tests de vistas y realizado InicioViewTests

--- app/tests/tests_views.py
+++ app/tests/tests_views.py
@@ -1,2 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from app.models import User,Usuario
+import json
 
+class InicioViewTests(TestCase):
+
+    @classmethod
+    def setUp(self):
+        #Instanciar objetos sin modificar que se usan en todos los métodos
+        user = User.objects.create(username='User1', password='pass')
+        usuario = Usuario.objects.create(nombre='Nombre1', apellidos='Apellidos', email='email@hotmail.com', email_academico='barranco@alum.us.es', titulacion='Titulación 1',descripcion='Descripcion 1', 
+        foto='foto.jpg', dinero=9.53, django_user=user)
+
+    # envia a la vista de login si el usuario no se ha autenticado
+    def test_not_logged(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'inicio.html')
+
+    # envia a la vista de sus cursos si el usuario esta autenticado
+    def test_logged(self):
+        client = Client()
+        client.force_login(User.objects.first())
+        response = client.get('/', follow=True)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'miscursos.html')
+
+class LoginViewTests(TestCase):
+
+    @classmethod
+    def setUp(self):
+        #Instanciar objetos sin modificar que se usan en todos los métodos
+        user = User.objects.create(username='User1', password='pass')
+        usuario = Usuario.objects.create(nombre='Nombre1', apellidos='Apellidos', email='email@hotmail.com', email_academico='barranco@alum.us.es', titulacion='Titulación 1',descripcion='Descripcion 1', 
+        foto='foto.jpg', dinero=9.53, django_user=user)
+
+    # muestra vista de login al hacer consulta get
+    def test_login_view(self):
+        #TODO
+        pass
