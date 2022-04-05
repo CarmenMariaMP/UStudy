dfac958fef065c16963061959c6414611825ff75
davbrican <72500941+davbrican@users.noreply.github.com>
2022-04-05 11:47:48 +0200

    test: Se crean tests de vistas

--- app/tests/tests_views.py
+++ app/tests/tests_views.py
@@ -39,3 +39,84 @@ class LoginViewTests(TestCase):
     def test_login_view(self):
         #TODO
         pass
+
+class PerfilUsuarioViewTests(TestCase):
+    
+    @classmethod
+    def setUp(self):
+        #Instanciar objetos sin modificar que se usan en todos los métodos
+        user = User.objects.create(username='User1', password='pass')
+        usuario = Usuario.objects.create(nombre='Nombre1', apellidos='Apellidos', email='email@hotmail.com', 
+                                         email_academico='barranco@alum.us.es', titulacion='Titulación 1',
+                                         descripcion='Descripcion 1', foto='foto.jpg', dinero=9.53, django_user=user)
+        
+    def test_profile_view(self):
+        client = Client()
+        client.force_login(User.objects.first())
+        response = client.get('/perfil', follow=True)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'perfil.html')
+        
+    def test_profile_view_not_logged(self):
+        client = Client()
+        response = client.get('/perfil', follow=True)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'login.html')
+        
+class InicioProfesotViewTests(TestCase):
+        
+    @classmethod
+    def setUp(self):
+        #Instanciar objetos sin modificar que se usan en todos los métodos
+        user = User.objects.create(username='User1', password='pass')
+        usuario = Usuario.objects.create(nombre='Nombre1', apellidos='Apellidos', email='email@hotmail.com', 
+                                         email_academico='barranco@alum.us.es', titulacion='Titulación 1',
+                                         descripcion='Descripcion 1', foto='foto.jpg', dinero=9.53, django_user=user)
+        
+    def test_profesor_view(self):
+        client = Client()
+        client.force_login(User.objects.first())
+        response = client.get('/inicio_profesor', follow=True)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'inicio_profesor.html')
+        
+    def test_profesor_view_not_logged(self):
+        client = Client()
+        response = client.get('/inicio_profesor', follow=True)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'login.html')
+
+class CrearCursoViewTests(TestCase):
+    
+    @classmethod
+    def setUp(self):
+        #Instanciar objetos sin modificar que se usan en todos los métodos
+        user = User.objects.create(username='User1', password='pass')
+        usuario = Usuario.objects.create(nombre='Nombre1', apellidos='Apellidos', email='email@hotmail.com', 
+                                         email_academico='barranco@alum.us.es', titulacion='Titulación 1',
+                                         descripcion='Descripcion 1', foto='foto.jpg', dinero=9.53, django_user=user)
+        
+        
+    def test_create_course_view(self):
+        client = Client()
+        client.force_login(User.objects.first())
+        response = client.get('/crearcurso', follow=True)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'crearcurso.html')
+        
+    '''
+    def test_create_course_post_view(self):
+        client = Client()
+        client.force_login(User.objects.first())
+        response = client.get('/crearcurso', follow=True)
+        response = client.post('/crearcurso/', data={"nombre": "Curso de prueba", "descripcion": "Esto es una descripción de prueba", "asignatura": "1"})
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'inicio_profesor.html')
+    '''
+    
+    def test_create_course_view_not_logged(self):
+        client = Client()
+        response = client.get('/crearcurso', follow=True)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response, 'inicio.html')
+        
