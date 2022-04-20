bfbdf044ecd6f341b496767bbc3ce75668ef3fba
davbrican <72500941+davbrican@users.noreply.github.com>
2022-04-20 20:59:41 +0200

    backend: Se arregla un error en la subida de archivos

--- app/views.py
+++ app/views.py
@@ -786,19 +786,19 @@ def dashboard_users(request):
         usuario_autenticado = request.user
         usuario = Usuario.objects.get(django_user=usuario_autenticado)
         cursos = usuario.Suscriptores
-        cursos_suscritos = list()
+        cursos_suscritos = []
         numero_valoraciones = 0
+        num_archivos = 0
+        numero_reportes = 0
+        num_susriptores = 0
+        valoracion_media_global = 0
 
         for c in cursos.all():
             curso = Curso.objects.get(id=c.id)
-            valoracion = get_valoracion(curso)
             cursos_suscritos.append(curso)
         
-        num_archivos = 0
-        numero_reportes = 0
         cursos_propietario = Curso.objects.filter(propietario = usuario)
-        num_susriptores = 0
-        valoracion_media_global = 0
+        
         for c in cursos_propietario:
             curso = Curso.objects.get(id=c.id)
             valoraciones = Valoracion.objects.all().filter(curso=curso)
@@ -814,11 +814,15 @@ def dashboard_users(request):
                 reportes.append(reporte)
             numero_reportes += len(reportes)
         
-        valoracion_media_global = valoracion_media_global / len(cursos_propietario)
+        try:
+            valoracion_media_global = valoracion_media_global / len(cursos_propietario)
+        except:
+            valoracion_media_global = 0.0
         
         return render(request, 'dashboard.html', {'usuario': usuario, 'cursos_suscritos': len(cursos_suscritos), 
                         'cursos_propietario': len(cursos_propietario), "suscriptores": num_susriptores, 'archivos_subidos': num_archivos,
                         'reportes': numero_reportes, 'valoraciones': numero_valoraciones, 'valoracion_media_global': valoracion_media_global})
+    return render(request, 'inicio.html')
 
 def subir_contenido(request):
     return render(request, "subir_contenido.html")
