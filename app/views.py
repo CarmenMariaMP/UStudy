9acac0708f2cf996b03a9cfebc42a6847861c8c8
JesusAparicioOrtiz <jesapaort@alum.us.es>
2022-04-24 13:53:52 +0200

    backend: Forms tests
    Se añaden tests de forms y se arregla la visualizacion de errores de pasarela de pago y de comentarios y respuestas

--- app/views.py
+++ app/views.py
@@ -737,6 +737,10 @@ def ver_archivo(request, id_curso, id_archivo):
                                                 alumno=usuario, descripcion=texto)
                     notificacion.save()
                     return redirect('/curso/'+str(id_curso)+'/archivo/'+str(id_archivo))
+                else:
+                    return render(request, "archivo.html", {'pdf': archivo.ruta, 'curso': curso, 'archivo': archivo, 'contenido_curso': contenido_curso, 'respuestasDict': respuestasDict,
+                                                    'acceso': acceso, 'comentarios': comentarios, 'url': url, 'formReporte': ReporteForm(), 'reportes': reportes, 'es_owner': es_owner,
+                                                    'usuario': usuario, 'es_plagio': es_plagio, 'es_error': es_error, 'formComentario': formComentario, 'formRespuesta': ResponderComentarioForm(), 'formRespuesta2': ResponderComentarioForm2()})
             elif request.POST['action'] == 'Responder':
                 formRespuesta = ResponderComentarioForm(request.POST)
                 if formRespuesta.is_valid():
@@ -746,6 +750,10 @@ def ver_archivo(request, id_curso, id_archivo):
                     Comentario.objects.create(texto=texto, archivo=archivo, fecha=datetime.datetime.now(
                     ), usuario=usuario, responde_a=Comentario.objects.get(id=responde_a))
                     return redirect('/curso/'+str(id_curso)+'/archivo/'+str(id_archivo))
+                else:
+                    return render(request, "archivo.html", {'pdf': archivo.ruta, 'curso': curso, 'archivo': archivo, 'contenido_curso': contenido_curso, 'respuestasDict': respuestasDict,
+                                                    'acceso': acceso, 'comentarios': comentarios, 'url': url, 'formReporte': ReporteForm(), 'reportes': reportes, 'es_owner': es_owner,
+                                                    'usuario': usuario, 'es_plagio': es_plagio, 'es_error': es_error, 'formComentario': ComentarioForm(), 'formRespuesta': formRespuesta, 'formRespuesta2': ResponderComentarioForm2()})
             elif request.POST['action'] == 'Responder2':
                 formRespuesta2 = ResponderComentarioForm2(request.POST)
                 if formRespuesta2.is_valid():
@@ -757,14 +765,14 @@ def ver_archivo(request, id_curso, id_archivo):
                     Comentario.objects.create(texto=texto, archivo=archivo, fecha=datetime.datetime.now(
                     ), usuario=usuario, responde_a=Comentario.objects.get(id=responde_a))
                     return redirect('/curso/'+str(id_curso)+'/archivo/'+str(id_archivo))
+                else:
+                    return render(request, "archivo.html", {'pdf': archivo.ruta, 'curso': curso, 'archivo': archivo, 'contenido_curso': contenido_curso, 'respuestasDict': respuestasDict,
+                                                    'acceso': acceso, 'comentarios': comentarios, 'url': url, 'formReporte': ReporteForm(), 'reportes': reportes, 'es_owner': es_owner,
+                                                    'usuario': usuario, 'es_plagio': es_plagio, 'es_error': es_error, 'formComentario': ComentarioForm(), 'formRespuesta': ResponderComentarioForm(), 'formRespuesta2': formRespuesta2})
         else:
-            formComentario = ComentarioForm()
-            formReporte = ReporteForm()
-            formRespuesta = ResponderComentarioForm()
-            formRespuesta2 = ResponderComentarioForm2()
             return render(request, "archivo.html", {'pdf': archivo.ruta, 'curso': curso, 'archivo': archivo, 'contenido_curso': contenido_curso, 'respuestasDict': respuestasDict,
-                                                    'acceso': acceso, 'comentarios': comentarios, 'url': url, 'formReporte': formReporte, 'reportes': reportes, 'es_owner': es_owner,
-                                                    'usuario': usuario, 'es_plagio': es_plagio, 'es_error': es_error, 'formComentario': formComentario, 'formRespuesta': formRespuesta, 'formRespuesta2': formRespuesta2})
+                                                    'acceso': acceso, 'comentarios': comentarios, 'url': url, 'formReporte': ReporteForm(), 'reportes': reportes, 'es_owner': es_owner,
+                                                    'usuario': usuario, 'es_plagio': es_plagio, 'es_error': es_error, 'formComentario': ComentarioForm(), 'formRespuesta': ResponderComentarioForm(), 'formRespuesta2': ResponderComentarioForm2()})
     else:
         return render(request, 'inicio.html')
 
